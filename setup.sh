9d5bab086e434e17119b6189bb37357c742a2413
Robert Bischoff <bischrob@gmail.com>
2023-09-20 07:42:28 -0700

    updated to remove web server

--- setup.sh
+++ setup.sh
@@ -5,3 +5,13 @@ docker exec -it casrshinysrvr-websrvr-1 bash "rm /etc/nginx/nginx.conf;"
 docker exec -it casrshinysrvr-websrvr-1 bash "ln -s /usr/local/nginx/conf/nginx.conf /etc/nginx/nginx.conf;"
 
 docker restart casrshinysrvr-websrvr-1;
+
+docker exec -it casrshinysrvr-websrvr-1 bash;
+
+rm /etc/nginx/nginx.conf;
+
+ln -s /usr/local/nginx/conf/nginx.conf /etc/nginx/nginx.conf;
+
+exit;
+
+docker restart casrshinysrvr-websrvr-1;
